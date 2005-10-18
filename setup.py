8d4607ebbde66325c3a560c1ea16f5108d113fd6
Federico Di Gregorio <fog@initd.org>
2005-10-18 14:20:27 +0000

    Releasing 2.0 beta 5.

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ from distutils.command.build_ext import build_ext
 from distutils.sysconfig import get_python_inc
 from distutils.ccompiler import get_default_compiler
 
-PSYCOPG_VERSION = '2.0rc1'
+PSYCOPG_VERSION = '2.0b5'
 version_flags   = []
 
 # to work around older distutil limitations
