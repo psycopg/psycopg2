0a73b75c5189f168a64e2583b30349f0978d113e
Federico Di Gregorio <fog@initd.org>
2005-10-25 16:00:49 +0000

    SUSE addition to setup.cfg.

--- sandbox/named.py
+++ sandbox/named.py
@@ -3,8 +3,8 @@ import psycopg2.extensions
 
 class Portal(psycopg2.extensions.cursor):
     def __init__(self, name, curs):
-        name = '"'+name+'"'
-        psycopg2.extensions.cursor.__init__(self, curs.connection, name)
+        psycopg2.extensions.cursor.__init__(
+            self, curs.connection, '"'+name+'"')
         
 CURSOR = psycopg2.extensions.new_type((1790,), "CURSOR", Portal)
 psycopg2.extensions.register_type(CURSOR)
@@ -13,9 +13,11 @@ conn = psycopg2.connect("dbname=test")
 
 curs = conn.cursor()
 curs.execute("SELECT reffunc2()")
+
 portal = curs.fetchone()[0]
 print portal.fetchone()
-portal.scroll(-1)
+print portal.fetchmany(2)
+portal.scroll(0, 'absolute')
 print portal.fetchall()
 
 
