0fc94be49647874123887f980368820f0a64191e
Federico Di Gregorio <fog@initd.org>
2009-08-09 17:22:18 +0200

    Preparing release 2.0.12

--- ZPsycopgDA/DA.py
+++ ZPsycopgDA/DA.py
@@ -18,7 +18,7 @@
 # See the LICENSE file for details.
 
 
-ALLOWED_PSYCOPG_VERSIONS = ('2.0.7','2.0.8','2.0.9','2.0.10', '2.0.11')
+ALLOWED_PSYCOPG_VERSIONS = ('2.0.7','2.0.8','2.0.9','2.0.10', '2.0.11', '2.0.12')
 
 import sys
 import time
