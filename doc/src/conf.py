b744c92f58ab3dfc65d51dfe61acc11427a192dd
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2010-02-14 16:11:01 +0000

    Documentation dir reordered.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -16,7 +16,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.append(os.path.abspath('.'))
+sys.path.append(os.path.abspath('tools/lib'))
 
 # -- General configuration -----------------------------------------------------
 
