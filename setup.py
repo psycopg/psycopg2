2212ea96a69dec8a0cbb60cccfbd48b244635249
Federico Di Gregorio <fog@initd.org>
2011-02-27 12:52:00 +0100

    Preparing release 2.4
    * NEWS file now uses 72 columns (better in emails and posts)
    * Bumped versions
    * Updated MonoDevelop file

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ except ImportError:
 # Take a look at http://www.python.org/dev/peps/pep-0386/
 # for a consistent versioning pattern.
 
-PSYCOPG_VERSION = '2.4-beta3'
+PSYCOPG_VERSION = '2.4'
 
 version_flags   = ['dt', 'dec']
 
