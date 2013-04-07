979c4fc1a619c5c93c541c94ba2480b488199fd7
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2013-04-07 18:29:28 +0100

    Bump to next maint release

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ except ImportError:
 # Take a look at http://www.python.org/dev/peps/pep-0386/
 # for a consistent versioning pattern.
 
-PSYCOPG_VERSION = '2.5'
+PSYCOPG_VERSION = '2.5.1.dev0'
 
 version_flags   = ['dt', 'dec']
 
