361522cde81b6e7ff4d3a4be444356559a0b9e50
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2013-04-21 21:42:05 +0100

    Added explicit Python 2 and Python 3 classifiers
    Not sure, but I suspect the "python3" page on PyPy in only based
    on the Language :: Python :: 3 classifier, not the more specific ones

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ Programming Language :: Python
 Programming Language :: Python :: 2.5
 Programming Language :: Python :: 2.6
 Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
 Programming Language :: Python :: 3.1
 Programming Language :: Python :: 3.2
 Programming Language :: Python :: 3.3
