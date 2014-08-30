00cafbe85bb82d85cbfe0f062a73c562433b5bc8
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2014-08-30 18:26:21 +0100

    Version changed for release 2.5.4

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ except ImportError:
 
 # Take a look at http://www.python.org/dev/peps/pep-0386/
 # for a consistent versioning pattern.
-PSYCOPG_VERSION = '2.5.4.dev0'
+PSYCOPG_VERSION = '2.5.4'
 
 version_flags   = ['dt', 'dec']
 
