0fa44ea9434b6c6e65c54622fc842844dcf2e8d1
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2016-03-08 05:13:57 +0000

    Start advertising Py 3.5 support

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ Programming Language :: Python :: 3.1
 Programming Language :: Python :: 3.2
 Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
 Programming Language :: C
 Programming Language :: SQL
 Topic :: Database
