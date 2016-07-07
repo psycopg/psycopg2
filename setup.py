1690fbace1bf498abd5a4c4f68bfaab609bc3587
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2016-07-07 02:56:27 +0100

    Version bumped for released 2.6.2

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ except ImportError:
 # Take a look at http://www.python.org/dev/peps/pep-0386/
 # for a consistent versioning pattern.
 
-PSYCOPG_VERSION = '2.6.2.dev0'
+PSYCOPG_VERSION = '2.6.2'
 
 version_flags   = ['dt', 'dec']
 
