651f1b6c97af8b82945a65bb46de4e9f4faf2438
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2017-01-03 19:12:44 +0100

    Consider Python 3.6 and Postgres 9.6 supported

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ Programming Language :: Python :: 3.2
 Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
 Programming Language :: C
 Programming Language :: SQL
 Topic :: Database
