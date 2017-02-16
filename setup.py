a3193aca2aa34b19da1ae3a11f413ebe22115528
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2017-02-16 19:52:58 +0000

    Bump version number

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ except ImportError:
 # Take a look at http://www.python.org/dev/peps/pep-0386/
 # for a consistent versioning pattern.
 
-PSYCOPG_VERSION = '2.7b2'
+PSYCOPG_VERSION = '2.7b3.dev0'
 
 
 # note: if you are changing the list of supported Python version please fix
