f54783ae6e6de44dafcecefdc8963c58a2ed98b9
My Karlsson <mk@acc.umu.se>
2017-10-26 20:11:51 +0200

    Emulate timeradd and timersub on Solaris
    Solaris does not have timeradd and timersub. Add solaris_support.c which
    provides emulated versions of them on Solaris.

--- setup.py
+++ setup.py
@@ -480,7 +480,7 @@ data_files = []
 sources = [
     'psycopgmodule.c',
     'green.c', 'pqpath.c', 'utils.c', 'bytes_format.c',
-    'libpq_support.c', 'win32_support.c',
+    'libpq_support.c', 'win32_support.c', 'solaris_support.c',
 
     'connection_int.c', 'connection_type.c',
     'cursor_int.c', 'cursor_type.c',
