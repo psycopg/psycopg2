296abf735ee236da7dcd78866156cdb0c1d3137e
Jon Dufresne <jon.dufresne@gmail.com>
2017-11-26 10:51:48 -0800

    Add some missing trove classifiers for general Python support

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ Intended Audience :: Developers
 License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
 License :: OSI Approved :: Zope Public License
 Programming Language :: Python
+Programming Language :: Python :: 2
 Programming Language :: Python :: 2.6
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
@@ -83,6 +84,7 @@ Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
+Programming Language :: Python :: Implementation :: CPython
 Programming Language :: C
 Programming Language :: SQL
 Topic :: Database
