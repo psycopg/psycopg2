8764a853204627ff8c0c23befa4bbaa5056b2c33
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2020-11-16 11:53:30 +0000

    Mention Python 3.9 supported
    See #1183

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
 Programming Language :: Python :: Implementation :: CPython
 Programming Language :: C
 Programming Language :: SQL
