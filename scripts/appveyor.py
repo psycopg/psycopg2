d956eaa3b1842a14895685be0d6d5e380fd029fc
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-11-17 22:53:44 +0200

    Drop support for EOL Python 2.7

--- scripts/appveyor.py
+++ scripts/appveyor.py
@@ -696,7 +696,7 @@ class Options:
     def py_ver(self):
         """The Python version to build as 2 digits string."""
         rv = os.environ['PY_VER']
-        assert rv in ('27', '36', '37', '38', '39'), rv
+        assert rv in ('36', '37', '38', '39'), rv
         return rv
 
     @property
@@ -779,11 +779,9 @@ class Options:
     def vs_ver(self):
         # https://wiki.python.org/moin/WindowsCompilers
         # https://www.appveyor.com/docs/windows-images-software/#python
-        # Py 2.7 = VS Ver. 9.0 (VS 2008)
         # Py 3.6--3.8 = VS Ver. 14.0 (VS 2015)
         # Py 3.9 = VS Ver. 16.0 (VS 2019)
         vsvers = {
-            '27': '9.0',
             '36': '14.0',
             '37': '14.0',
             '38': '14.0',
