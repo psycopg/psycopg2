5343dff5adf3397c88915e963c90e72d5dc32a8f
FelipeV34 <felipevanegas11@hotmail.com>
2021-08-19 00:39:03 -0500

    Create contextmanagers.py
    Context managers for a connection, a cursor given a connection, and a cursor given the db connection kwargs

---
+++ lib/contextmanagers.py
@@ -0,0 +1,49 @@
+"""psycopg context managers for connections, cursors, and pools."""
+
+
+from contextlib import contextmanager
+from extensions import connect, cursor
+
+
+@contextmanager
+def connection(**kwargs):
+    """Context manager to yield and close a db connection."""
+    connection = connect(**kwargs)
+
+    try:
+        yield connection
+
+    finally:
+        connection.close()
+
+
+@contextmanager
+def cursor(connection):
+    """
+    Context manager to yield and close a cursor given an existing db
+    connection.
+    """
+    cursor = connection.cursor()
+
+    try:
+        yield cursor
+
+    finally:
+        cursor.close()
+
+
+@contextmanager
+def connection_cursor(**kwargs):
+    """
+    Context manager to yield a cursor and close it and its connection for
+    purposes or performing a single db call.
+    """
+    connection = connect(**kwargs)
+    cursor = connection.cursor()
+
+    try:
+        yield cursor
+
+    finally:
+        cursor.close()
+        connection.close()
