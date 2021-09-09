c551f2e02e4160c39b771e6c37115295e078da57
Armen Zambrano G <armenzg@sentry.io>
2021-09-09 09:34:11 -0400

    More formatting

--- scripts/build/build_macos.sh
+++ scripts/build/build_macos.sh
@@ -19,8 +19,8 @@ brew services start postgresql
 for i in $(seq 10 -1 0); do
   eval pg_isready && break
   if [ $i == 0 ]; then
-        echo "PostgreSQL service not ready, giving up"
-        exit 1
+      echo "PostgreSQL service not ready, giving up"
+      exit 1
   fi
   echo "PostgreSQL service not ready, waiting a bit, attempts left: $i"
   sleep 5
