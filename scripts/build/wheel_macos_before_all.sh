97df29a312f574d1e24875f67e1fcc73b32a2f53
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2023-03-30 02:39:21 +0200

    ci: build macOS packages using cibuildwheel
    Close #1558.

---
+++ scripts/build/wheel_macos_before_all.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Configure the environment needed to build wheel packages on Mac OS.
+# This script is designed to be used by cibuildwheel as CIBW_BEFORE_ALL_MACOS
+
+set -euo pipefail
+set -x
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+prjdir="$( cd "${dir}/../.." && pwd )"
+
+brew install gnu-sed postgresql@15
+
+# Start the database for testing
+brew services start postgresql
+
+# Wait for postgres to come up
+for i in $(seq 10 -1 0); do
+  eval pg_isready && break
+  if [ $i == 0 ]; then
+      echo "PostgreSQL service not ready, giving up"
+      exit 1
+  fi
+  echo "PostgreSQL service not ready, waiting a bit, attempts left: $i"
+  sleep 5
+done
+
+# Replace the package name
+if [[ "${PACKAGE_NAME:-}" ]]; then
+    gsed -i "s/^setup(name=\"psycopg2\"/setup(name=\"${PACKAGE_NAME}\"/" \
+        "${prjdir}/setup.py"
+fi
