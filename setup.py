e1cf23d9c74ef520ead11fcacc73d17b8754ed00
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-10-05 01:40:45 -0600

    Drop Python 3.7 in other places

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ Intended Audience :: Developers
 License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
 Programming Language :: Python
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
 Programming Language :: Python :: 3.10
