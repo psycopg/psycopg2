4ce9cc168c497fa94566d5c2547bbec4e1bf7462
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2025-10-09 01:46:29 +0200

    chore: drop support for Python 3.8

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ Intended Audience :: Developers
 License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
 Programming Language :: Python
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
 Programming Language :: Python :: 3.10
 Programming Language :: Python :: 3.11
@@ -524,7 +523,7 @@ setup(name="psycopg2",
       url="https://psycopg.org/",
       license="LGPL with exceptions",
       platforms=["any"],
-      python_requires='>=3.8',
+      python_requires='>=3.9',
       description=readme.split("\n")[0],
       long_description="\n".join(readme.split("\n")[2:]).lstrip(),
       classifiers=[x for x in classifiers.split("\n") if x],
